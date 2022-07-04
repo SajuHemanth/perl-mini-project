69f5a5100c6c186d3239c1c2c3e1e96ea84ea0c1
SajuHemanth <96937846+SajuHemanth@users.noreply.github.com>
2022-07-04 17:58:42 +0530

    Add files via upload

---
+++ demo project(04-07-22)/FlightBooking.pl
@@ -0,0 +1,35 @@
+use lib "/home/asplap1195/Desktop/DemoProject";
+use strict;
+use warnings;
+use LoginDetail;
+print "->Welcome To Foresight Airlines<-\n";
+print "********************************\n";
+my $input;
+my $std=1;
+while($std==1) {
+    print ("\nFurther Details Regarding Foresight Airlines are provided as follows:\n");
+    print ("-----------------------------------------------------------------------\n");
+    print ("please choose any of the following options: \n");
+    print ("1.Login Details  \n");
+    print ("2.New Registration   \n");
+    print ("3.Steps   \n");
+    print ("4.User Manual   \n");
+    print ("5.Availability chart  \n");
+    print ("6.Booking Page \n");
+    print ("7.View Details\n");
+    print ("8.Cancellation policy\n");
+    print ("9.History of booking\n");
+    print ("10.Exit Page\n");
+    print("Enter any key:\n");
+    $input =<STDIN>;
+    if ($input =~ /^[1-6]$/) {
+       if($input==1) {
+            LoginDetail->login_user();
+            $std=0;
+        } 
+    }
+    else {
+            print("Enter correct key:");
+	    }
+}
+1;

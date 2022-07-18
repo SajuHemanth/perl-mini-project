675d82bac95a16f826f1e525239e7289e8caa88e
SajuHemanth <96937846+SajuHemanth@users.noreply.github.com>
2022-07-18 20:45:11 +0530

    Add files via upload

---
+++ Todays task/18-07-22/FlightBooking.pl
@@ -0,0 +1,78 @@
+use lib "/home/asplap1195/Desktop/DemoProject";
+use strict;
+use warnings;
+use LoginDetail;
+use Validations;
+use DbConnection;
+my $connection= $DbConnection::dataConnection;
+print "->Welcome To Foresight Airlines<-\n";
+print "-----------------------------------\n";
+my $input;
+my $std=1;
+while($std==1) {
+    print ("\nFurther Details Regarding Foresight Airlines are provided as follows:\n");
+    print ("-----------------------------------------------------------------------\n");
+    print ("please choose any of the following options: \n");
+    print ("1.Login Details \n");
+    print ("2.New Registration \n");
+    print ("3.User Manual \n");
+    print ("4.Availability chart \n");
+    print ("5.Booking Page \n");
+    print ("6.View Details \n");
+    print ("7.Cancellation policy \n");
+    print ("8.History of booking \n");
+    print ("9.Exit Page \n");
+    print("Enter any key:");
+    my $input =<STDIN>;
+    if ($input =~ /\d+/) {
+        if($input == 1)
+        {
+            my $login_user = LoginDetail->new;
+            my $User_menu = LoginDetail->menu;
+        }
+        elsif($input == 2)
+        {
+            my $detail = LoginDetail->user_detail;
+            my $User_menu = LoginDetail->menu;
+        }
+        elsif($input == 3) 
+        {
+             my $User_manual = LoginDetail->user_manual;
+             my $User_menu = LoginDetail->menu;
+        }
+        elsif($input == 4)
+        {
+             my $Availability = LoginDetail->availability_chart;
+             my $User_menu = LoginDetail->menu;
+        }
+        elsif($input == 5)
+        {
+             my $Booking = LoginDetail->booking_page;
+             my $User_menu = LoginDetail->menu;
+        }
+        elsif($input == 6)
+        {
+             my $Viewdetails = LoginDetail->view_details;
+             my $User_menu = LoginDetail->menu;
+        }
+        elsif($input == 7)
+        {
+             my $Cancellation = LoginDetail->cancellation_policy;
+             my $User_menu = LoginDetail->menu;
+        }
+        elsif($input == 8)
+        {
+             my $Booking = LoginDetail->booking_history;
+             my $User_menu = LoginDetail->menu;
+        }
+        elsif($input == 9)
+        {
+             my $exit= LoginDetail->exitpage;
+            $std=0;
+        }
+        else
+        {
+            printf "Enter the Correct Key:\n";
+        }
+    }
+}
---
+++ Todays task/18-07-22/Validations.pm
@@ -0,0 +1,117 @@
+package Validations;
+use lib "/home/asplap1195/Desktop/DemoProject";
+use strict;
+use warnings;
+use DbConnection;
+use Config;
+
+sub validate_name
+{
+    my $class = shift;
+    my $UserName = shift;
+    if ($UserName eq '') {
+       print($config::error_name{1});
+       return 'false';
+    }
+    if ($UserName =~ /^([A-z ]*)$/) {
+       return 'true';
+    }
+    else {
+       print("Please enter the name in correct format\n");
+       return 'false';
+    }
+}   
+
+sub validate_password
+{
+   my $class = shift;
+   my $password = shift;
+   if ($password eq '') {
+       print("Please enter the detail,input cannot be empty\n");
+       return 'false';
+   }
+   if ($password =~ /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/) {
+       return 'true';
+    }
+    else {
+       print("Please enter the valid password\n");
+       return 'false';
+   }
+
+}
+
+sub validate_email
+{
+    my $class = shift;
+    my $email_id = shift;
+    if ($email_id eq '') {
+       print("Please enter the detail,input cannot be empty\n");
+       return 'false';
+    }
+    if ($email_id =~ /^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/) {
+        return 'true';
+    }
+    else {
+        print("please enter the valid emailId\n");
+        return 'false';
+    }
+}
+
+sub validate_gender
+{
+    my $class = shift;
+    my $gender = shift;
+    if ($gender eq '') {
+        print("Please enter the detail,input cannot be empty\n");
+        return 'false';
+    }
+    if ($gender =~  m{^[MFTmft]} and length($gender) == 1) {
+        return 'true';
+    }
+    else {
+        print("Please enter the Correct Gender \n");
+        return 'false';
+    }
+    
+}
+
+sub validate_phoneNumber
+{
+    my $class = shift;
+    my $phone_no = shift;
+    if ($phone_no eq '') {
+       print("Please enter the detail,input cannot be empty\n");
+       return 'false';
+    }
+    if ($phone_no =~  m{^[6-9]}) {
+        if(length($phone_no) < 10 or length($phone_no) > 10) {
+            print("Phone number should be 10 digit\n");
+            return 'false';
+        }
+        return 'true';
+    }
+    else {
+        print("Please enter the valid Phone Number \n");
+        return 'false';
+    }
+}
+
+sub Validate_Userid
+      {
+    my $class = shift;
+    my $UserId = shift;
+    if ($UserId eq '') {
+        print("Input cannot be ampty\n");
+        return 'false';
+    }
+    if (length($UserId) == 6 and $UserId =~  m{FSA[0-9]{3}}) {
+        return 'true';
+    }
+    else {
+       print("Enter the valid id\n");
+       return 'false';
+    }
+    
+}
+
+1; 
